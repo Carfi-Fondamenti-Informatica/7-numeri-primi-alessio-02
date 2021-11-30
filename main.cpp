d7dcc08dad074637098b0de9c75566ed093b25b9
alessio-02 <91601426+alessio-02@users.noreply.github.com>
2021-11-30 17:44:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+int main() {
+    int num=0;
+    std::cin>>num;
+    if(verifica(num)== true) {
+    std::cout << "numero primo" << std::endl;
+    }
+    if(verifica(num)== false){
+    std::cout << "numero non primo" << std::endl;
+    }
+        return 0;
 }
