27e92d55ca9d5bf07e2154a695f93875a8320f4d
alessio-02 <91601426+alessio-02@users.noreply.github.com>
2021-11-30 17:43:45 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
+bool verifica(int &num) {
+    int conta = 0, div = 1;
+
+    while (conta < 3 and div <= num / 2) {
+        if (num % div == 0){
+            conta = conta + 1;
+        }
+        div = div + 1;
+    }
+    if (conta == 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
